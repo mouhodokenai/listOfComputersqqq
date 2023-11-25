a0599460b9f9b58e4e757959a4d5caef6164041e
marie <122727409+mouhodokenai@users.noreply.github.com>
2023-11-25 11:43:29 +0300

    что-то пытаюсь

--- app/src/main/java/com/example/listofcomputers/MainActivity.java
+++ app/src/main/java/com/example/listofcomputers/MainActivity.java
@@ -1,27 +1,46 @@
 package com.example.listofcomputers;
 
+import static com.example.listofcomputers.MainActivity.adapter;
+import static com.example.listofcomputers.MainActivity.dataItems;
+import static com.example.listofcomputers.MainActivity.serverAccessor;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.Manifest;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
+    private static ListView listView;
     static ArrayAdapter adapter;
     static ArrayList<Computer> dataItems;
     private Fragment_info infoFragment;
     private Fragment_add addFragment;
-
+    private static final int MY_PERMISSIONS_REQUEST_INTERNET = 123; // или любое другое уникальное число
+    private static final String SERVICE_ADDRESS = "http://37.77.105.18/api/Desktops";
+    public static ServerAccessor serverAccessor = new ServerAccessor(SERVICE_ADDRESS);
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,12 +55,22 @@ public class MainActivity extends AppCompatActivity {
         infoFragment = new Fragment_info();
         addFragment = new Fragment_add();
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Разрешение не предоставлено, запросить его у пользователя
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_INTERNET);
+        }
 
+        //Запуск фоновой задачи
+        ProgressTask progressTask = new ProgressTask();
+        executorService.submit(progressTask);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+/*
         // Инициализация данных из базы данных
         DataBaseAccessor databaseAccessor = new DataBaseAccessor(this);
         dataItems.addAll(databaseAccessor.getAllData());
-
+ */
         adapter.notifyDataSetChanged();
 
         // Обработчик кнопки "Добавить"
@@ -82,11 +111,44 @@ public class MainActivity extends AppCompatActivity {
             transaction.commit();
         });
     }
+    public ArrayAdapter<String> AdapterUpdate(ArrayList<Computer> list) {
+        ArrayList<String> stringList = serverAccessor.getStringListFromNoteList(list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataItems);
+        // установить адаптер в listview
+        MainActivity.listView.setAdapter(adapter);
+        return adapter;
+    }
 
     // Метод для определения, является ли устройство планшетом
     public static boolean isTablet(Context context) {
         return (context.getResources().getConfiguration().screenLayout
                 & Configuration.SCREENLAYOUT_SIZE_MASK)
                 >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+}
+class ProgressTask implements Runnable {
+    String connectionError = null;
+
+    @Override
+    public void run() {
+        try {
+            // выполнение в фоне
+            dataItems = serverAccessor.getData();
+
+            // Обновление UI осуществляется в основном потоке
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (connectionError == null) {
+                        adapter = AdapterUpdate(dataItems);
+                    } else {
+                        //проблемы с интернетом
+                    }
+                }
+            });
+
+        } catch (Exception ex) {
+            connectionError = ex.getMessage();
+        }
     }
 }
